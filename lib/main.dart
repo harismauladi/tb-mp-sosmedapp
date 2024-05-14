6143564ae5e2fede8ee71ba3bc7bcc2fbe3e2346
anonymous@bti.id <anonymous>
2024-05-14 11:20:18 +0700

    Add Member Name in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,11 @@ void main() async {
   
   runApp(MyApp());
 }
+
+/***
+ * ANGGOTA KELOMPOK
+ * - HARIS MAULADI 41522110064
+ * - Ridwan 41522110079
+ * - Muhammad Thariq Alwan Hafizh 41522110056
+ * - Rahmat alpian  41522110010
+ */
